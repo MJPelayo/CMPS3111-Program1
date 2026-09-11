8a4111cea28928966986d01afce8d09d35142166
Marlon Pelayo <2021153565@ub.edu.bz>
2026-09-10 21:54:55 -0600

    Add initial working C# program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("CMPS 3111 Language Recognizer");
+        Console.WriteLine("Program starting...");
+    }
+}
